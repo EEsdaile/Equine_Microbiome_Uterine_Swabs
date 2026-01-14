2036821a057a24d62ade5856b368d29e59e658d7
Elizabeth Esdaile <56132460+EEsdaile@users.noreply.github.com>
2026-01-14 13:06:44 -0700

    modified 01.demultiplex for alpine

--- 01.sh
+++ 01.sh
@@ -5,6 +5,7 @@
 #SBATCH --ntasks=4
 #SBATCH --partition=amilan
 #SBATCH --time=24:00:00
+#SBATCH --qos=normal
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=e.s.esdaile@colostate.edu
 #SBATCH --output=01.demultiplex_%j.out
@@ -15,7 +16,7 @@ conda activate qiime2-amplicon-2024.10
 
 snakemake \
         -c 4 \
-        -s 01.demultiplex \
+        -s 01.demultiplex_alpine \
         --verbose \
         --printshellcmds
 
