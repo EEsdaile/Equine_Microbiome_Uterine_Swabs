d4eda425339f2a45669fc4b2c8d755145c2bb2f1
Elizabeth Esdaile <56132460+EEsdaile@users.noreply.github.com>
2026-02-07 10:16:35 -0700

    updated 03.taxonomy_greengenes to have multiple filtering versions by number of samples and reads. Created script for ancom analysis

--- 04.sh
+++ 04.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name=03.taxonomy_greengenes
-#SBATCH --nodes=4
-#SBATCH --ntasks=8
+#SBATCH --job-name=04.ancom
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
 #SBATCH --partition=amilan
 #SBATCH --time=24:00:00
 #SBATCH --qos=normal
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=e.s.esdaile@colostate.edu
-#SBATCH --output=03.taxonomy_greengenes_%j.out
+#SBATCH --output=04.ancom_%j.out
 
-#to classify features, use 8 nodes and 64 tasks. same with taxonomy step that uses sepp file.
+#to classify features, use 8 nodes and 64 tasks. 
 
 module load anaconda
 
@@ -18,7 +18,7 @@ conda activate qiime2-amplicon-2024.10
 
 snakemake \
         -c 4 \
-        -s 03.taxonomy_greengenes \
+        -s 04.ancom \
         --verbose \
         --printshellcmds
 
